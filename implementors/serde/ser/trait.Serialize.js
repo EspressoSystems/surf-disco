59be69fcec7615b68bc463042af23a0f8a2f3144
Ancient123 <Ancient123@users.noreply.github.com>
2023-12-12 05:55:55 +0000

    deploy: 1406bf37da4824a26835afce0092b2b3f74b3dd9

--- implementors/serde/ser/trait.Serialize.js
+++ implementors/serde/ser/trait.Serialize.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"surf_disco":[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.192/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"enum\" href=\"surf_disco/socket/enum.Unsupported.html\" title=\"enum surf_disco::socket::Unsupported\">Unsupported</a>"]]
+"surf_disco":[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.193/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"enum\" href=\"surf_disco/socket/enum.Unsupported.html\" title=\"enum surf_disco::socket::Unsupported\">Unsupported</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
