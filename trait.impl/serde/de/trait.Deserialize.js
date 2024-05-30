740ccc84e6993545ef53e2c6c6b9f9f2eb203601
jbearer <jbearer@users.noreply.github.com>
2024-05-30 00:23:45 +0000

    deploy: 025ea97a384a3acd2ffad80c365b0b908d1811ad

--- trait.impl/serde/de/trait.Deserialize.js
+++ trait.impl/serde/de/trait.Deserialize.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"surf_disco":[["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.197/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"enum\" href=\"surf_disco/socket/enum.Unsupported.html\" title=\"enum surf_disco::socket::Unsupported\">Unsupported</a>"]]
+"surf_disco":[["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.203/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"enum\" href=\"surf_disco/socket/enum.Unsupported.html\" title=\"enum surf_disco::socket::Unsupported\">Unsupported</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
