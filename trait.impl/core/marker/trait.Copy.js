096427f92b3295a25d51428f7a96e3aa7de4e2d0
imabdulbasit <imabdulbasit@users.noreply.github.com>
2025-06-26 13:20:32 +0000

    deploy: dad538768f27f45328517c5305bf3b95ee7f4650

--- trait.impl/core/marker/trait.Copy.js
+++ trait.impl/core/marker/trait.Copy.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["surf_disco",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"surf_disco/client/enum.ContentType.html\" title=\"enum surf_disco::client::ContentType\">ContentType</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"surf_disco/socket/enum.Unsupported.html\" title=\"enum surf_disco::socket::Unsupported\">Unsupported</a>"]]]]);
+    var implementors = Object.fromEntries([["surf_disco",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"surf_disco/client/enum.ContentType.html\" title=\"enum surf_disco::client::ContentType\">ContentType</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"surf_disco/socket/enum.Unsupported.html\" title=\"enum surf_disco::socket::Unsupported\">Unsupported</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
